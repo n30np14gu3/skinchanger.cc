6db3c7b102b04eb7eb469df6681bb1707747f384
n30np14gu3 <darknetsoft@yandex.ru>
2020-01-27 17:18:20 +0300

    Design complete

--- skinchanger_loader/Properties/Resources.Designer.cs
+++ skinchanger_loader/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace skinchanger_loader.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -67,16 +67,6 @@ namespace skinchanger_loader.Properties {
             get {
                 object obj = ResourceManager.GetObject("_mmi_", resourceCulture);
                 return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap header_image {
-            get {
-                object obj = ResourceManager.GetObject("header_image", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
